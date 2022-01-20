610faf4eccd8785d42ebfbf1acaf33cf325ca3e5
rudolf0328 <ck0716080@gmail.com>
2022-01-20 16:13:52 +0900

    scroll

---
+++ disneyweb/src/MainPage/main.js
@@ -0,0 +1,32 @@
+var Animation = function () {
+  var items, winH;
+ 
+  var initModule = function () {
+    items = document.querySelectorAll(".text");
+    winH = window.innerHeight;
+    _addEventHandlers();
+  }
+ 
+  var _addEventHandlers = function () {
+    window.addEventListener("scroll", _checkPosition);
+    window.addEventListener("load", _checkPosition);
+    window.addEventListener("resize", initModule);
+  };
+ 
+  var _checkPosition = function () {
+    for (var i = 0; i < items.length; i++) {
+      var posFromTop = items[i].getBoundingClientRect().top;
+      if (winH > posFromTop) {
+        items[i].classList.add("active");
+      }
+    }
+  }
+ 
+  return {
+    init: initModule
+  }
+}
+ 
+animation().init();
+
+export default Animation;
--- disneyweb/src/index.js
+++ disneyweb/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Animation from './MainPage/main';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <animation/> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
