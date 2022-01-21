f32f1621c56f6af83aa497c63b05c95b51ed2c9a
rudolf0328 <ck0716080@gmail.com>
2022-01-21 16:51:31 +0900

    app.js

--- disneyweb/src/App.js
+++ disneyweb/src/App.js
@@ -2,29 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import MainPage from './MainPage/main';
 import Introduce from './MainPage/main';
-import Header from './Header/main';
 import { Routes, Route, BrowserRouter as Router} from "react-router-dom";
-
 function App() {
   return (
     <Router>
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <MainPage/> */}
-      <Header></Header>
+      <Routes>
+      <Route path = "/" element = {<MainPage/>} />
+      <Route path = "/home" element = {<MainPage/>} />
+      </Routes>
     </div>
     </Router>
   );
