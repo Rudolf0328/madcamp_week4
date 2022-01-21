e98eaffc38ccfbdb618eb40887575b39761451d8
wkcho99 <wkcho@postech.ac.kr>
2022-01-21 23:49:09 +0900

    record

--- disneyweb/src/App.js
+++ disneyweb/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import MainPage from './MainPage/main';
 import Introduce from './MainPage/main';
+import NeedleDrop from './needledrop/needledrop';
 import { Routes, Route, BrowserRouter as Router} from "react-router-dom";
 function App() {
   return (
@@ -23,7 +24,7 @@ function App() {
       </header> */}
       <Routes>
       <Route path = "/" element = {<MainPage/>} />
-      <Route path = "/home" element = {<MainPage/>} />
+      <Route path = "/home" element = {<NeedleDrop/>} />
       </Routes>
     </div>
     </Router>
