9725439cc9a945faaca8e9d38756b3d96549dc8a
rudolf0328 <ck0716080@gmail.com>
2022-01-21 23:55:52 +0900

    update changes

--- disneyweb/src/App.js
+++ disneyweb/src/App.js
@@ -1,45 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-// import MainPage from './MainPage/main';
-// import Introduce from './MainPage/main';
-import InfoPage from './InfoPage/main'
-import InfoPage2 from './Infopage2/main'
-
-=======
 import MainPage from './MainPage/main';
 import Introduce from './MainPage/main';
 import { Routes, Route, BrowserRouter as Router} from "react-router-dom";
 import Header from './Header/main';
->>>>>>> 362e00ca85eaef6537ea9d38e814fe009463c530
 
 function App() {
   return (
     <Router>
     <div className="App">
-<<<<<<< HEAD
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <InfoPage/>
-=======
       <Routes>
       <Route path = "/" element = {<MainPage/>} />
       <Route path = "/home" element = {<MainPage/>} />
       <Route path = "/header" element = {<Header/>} />
       </Routes>
->>>>>>> 362e00ca85eaef6537ea9d38e814fe009463c530
     </div>
     </Router>
   );
