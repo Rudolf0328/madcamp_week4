175dee3cfe76f3ea29ce65e65378f2e6120609da
rudolf0328 <ck0716080@gmail.com>
2022-01-22 00:13:07 +0900

    update changes

--- disneyweb/src/App.js
+++ disneyweb/src/App.js
@@ -1,11 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-=======
 
 import InfoPage from './InfoPage/main'
 
->>>>>>> 26c2c0045021241440291c80865c71daf3c255d1
 import MainPage from './MainPage/main';
 import Introduce from './MainPage/main';
 import NeedleDrop from './needledrop/needledrop';
@@ -20,6 +17,7 @@ function App() {
       <Route path = "/" element = {<MainPage/>} />
       <Route path = "/home" element = {<NeedleDrop/>} />
       <Route path = "/header" element = {<Header/>} />
+      <Route path = "/info" element = {<InfoPage/>} />
       </Routes>
     </div>
     </Router>
