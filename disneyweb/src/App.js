362e00ca85eaef6537ea9d38e814fe009463c530
rudolf0328 <ck0716080@gmail.com>
2022-01-21 23:42:27 +0900

    header

--- disneyweb/src/App.js
+++ disneyweb/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import MainPage from './MainPage/main';
 import Introduce from './MainPage/main';
 import { Routes, Route, BrowserRouter as Router} from "react-router-dom";
+import Header from './Header/main';
+
 function App() {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App() {
       <Routes>
       <Route path = "/" element = {<MainPage/>} />
       <Route path = "/home" element = {<MainPage/>} />
+      <Route path = "/header" element = {<Header/>} />
       </Routes>
     </div>
     </Router>
--- disneyweb/src/Header/main.js
+++ disneyweb/src/Header/main.js
@@ -1,12 +1,47 @@
 import { Box } from "@mui/system";
 import { Component } from "react";
 import './main.css';
+import { Grid, Link, Typography } from "@mui/material";
 
 class Header extends Component {
   render() {
-    return<Box className="container">
-      <img src="./diswiki.png" className="logo-diswiki"> </img>
-    </Box>
+    return<div className='container' alignItems="center" justifyContent='space-evenly' className='menu' flexDirection='row' spacing={2} sx={{
+      // width: '100%',
+      // height: '15%',
+      padding: '2.5%',
+      height: "157px",
+      backgroundColor: 'white',
+    }} className="header">
+      <div className='item' alignItems="flex-start">
+          <Link to="/home">
+            <img src="./diswiki.png" align='left' width='241px' height='88.86px'></img>
+          </Link>
+      </div>
+      <div className='item'>
+        <a>
+          <Typography color="black" fontSize='24px' align='center' sx={{ fontWeight: 700 }}>작품</Typography>
+        </a>
+      </div>
+      <div className='item'>
+        <a>
+          <Typography color="black" fontSize='24px' align='center' sx={{ fontWeight: 700 }}>음악</Typography>
+        </a>
+      </div>
+      <div className='item'>
+        <a>
+          <Typography color="black" fontSize='24px' align='center' sx={{ fontWeight: 700 }}>미니게임</Typography>
+        </a>
+      </div>
+      
+      <div className='item' xs className='container' alignItems='center'>
+        <div className='item'>
+          <Typography color="black" fontSize='18px' align='right' sx={{ fontWeight: 700 }}>공듀</Typography>
+        </div>
+        <div className='item'>
+          <img className="round-image"></img>
+        </div>
+      </div>
+    </div>
   }
 }
 
