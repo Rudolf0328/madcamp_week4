4159c2fb9c36913da383084140e347e871916bd2
rudolf0328 <ck0716080@gmail.com>
2022-01-22 15:05:29 +0900

    underline

--- disneyweb/src/Header/main.js
+++ disneyweb/src/Header/main.js
@@ -1,47 +1,77 @@
 import { Box } from "@mui/system";
 import { Component } from "react";
 import './main.css';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import { Grid, Link, Typography } from "@mui/material";
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+// import { Tooltip, tooltipClasses } from "@material-ui/core";
+
+const CustomWidthTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    width: 137,
+    // color: '#191C83'
+  },
+});
+
+const theme = createTheme({
+  components: {
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          backgroundColor: "#191C83"
+        }
+      }
+    }
+  }
+});
 
 class Header extends Component {
   render() {
-    return<div className='container' alignItems="center" justifyContent='space-evenly' className='menu' flexDirection='row' spacing={2} sx={{
+    return<Grid container alignItems="center" className="menu" name="hover-feedback" justifyContent='space-evenly' flexDirection='row' spacing={2} sx={{
       // width: '100%',
       // height: '15%',
       padding: '2.5%',
       height: "157px",
       backgroundColor: 'white',
     }} className="header">
-      <div className='item' alignItems="flex-start">
+      <Grid item xs alignItems="flex-start">
           <Link to="/home">
             <img src="./diswiki.png" align='left' width='241px' height='88.86px'></img>
           </Link>
-      </div>
-      <div className='item'>
-        <a>
-          <Typography color="black" fontSize='24px' align='center' sx={{ fontWeight: 700 }}>작품</Typography>
-        </a>
-      </div>
-      <div className='item'>
-        <a>
-          <Typography color="black" fontSize='24px' align='center' sx={{ fontWeight: 700 }}>음악</Typography>
-        </a>
-      </div>
-      <div className='item'>
-        <a>
-          <Typography color="black" fontSize='24px' align='center' sx={{ fontWeight: 700 }}>미니게임</Typography>
-        </a>
-      </div>
+      </Grid>
+      <Grid item xs>
+        <ThemeProvider theme={theme}>
+          <CustomWidthTooltip>
+            <Typography color="black" fontSize='24px' align='center' variant="overline" sx={{ fontWeight: 700 }}>작품</Typography>
+          </CustomWidthTooltip>
+        </ThemeProvider>
+      </Grid>
+      <Grid item xs>
+      <ThemeProvider theme={theme}>
+          <CustomWidthTooltip>
+            <Typography color="black" fontSize='24px' align='center' variant="overline" sx={{ fontWeight: 700 }}>음악</Typography>
+          </CustomWidthTooltip>
+        </ThemeProvider>
+      </Grid>
+      <Grid item xs>
+      <ThemeProvider theme={theme}>
+          <CustomWidthTooltip>
+            <Typography color="black" fontSize='24px' align='center' variant="overline" sx={{ fontWeight: 700 }}>미니게임</Typography>
+          </CustomWidthTooltip>
+        </ThemeProvider>
+      </Grid>
       
-      <div className='item' xs className='container' alignItems='center'>
-        <div className='item'>
+      <Grid item xs container alignItems='center'>
+        <Grid item xs>
           <Typography color="black" fontSize='18px' align='right' sx={{ fontWeight: 700 }}>공듀</Typography>
-        </div>
-        <div className='item'>
+        </Grid>
+        <Grid item xs>
           <img className="round-image"></img>
-        </div>
-      </div>
-    </div>
+        </Grid>
+      </Grid>
+    </Grid>
   }
 }
 
