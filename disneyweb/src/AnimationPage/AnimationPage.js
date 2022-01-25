397d67a1a602de6b728a0232abfbb54d24f4e2f4
rudolf0328 <ck0716080@gmail.com>
2022-01-26 05:05:56 +0900

    animation page

--- disneyweb/src/AnimationPage/AnimationPage.js
+++ disneyweb/src/AnimationPage/AnimationPage.js
@@ -1,28 +1,70 @@
 import { Component } from "react";
+import "./animation.css"
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+const itemData = [
+  {
+    img: "./aladin.jpeg",
+    title: "Aladin"
+  },
+  {
+    img: "./thelittlemermaid.jpg",
+    title: "The Little Mermaid"
+  },
+  {
+    img: "./cinderella.jpeg",
+    title: "Cinderella"
+  },
+  {
+    img: "./beautyandthebeast_poster.jpg",
+    title: "Beauty And The Beast"
+  },
+  {
+    img: "./frozen_poster.jpg",
+    title: "Frozen1"
+  },
+  {
+    img: "./frozen2_poster.jpeg",
+    title: "Frozen2"
+  },
+  {
+    img: "./moana_poster.jpg",
+    title: "Moana"
+  },
+  {
+    img: "./mulan_poster.jpg",
+    title: "Mulan"
+  },
+  {
+    img: "./tangled.jpeg",
+    title: "Tangled"
+  },
+  {
+    img: "./pochahontas.jpg",
+    title: "Pochahontas"
+  }
+]
 
 class AnimationPage extends Component {
   render() {
-    return(<div className="container-poster">
-
-    <img src="https://assets.codepen.io/1025350/schitts-creek.png" alt="the cast of Schitt’s Creek" />
-  
-    <div className="poster">
-      <img src="https://assets.codepen.io/1025350/schitts-creek.png" alt="the cast of Schitt’s Creek" />
+    return(
+    <div className="animation-page-background">
+      <ImageList sx={{ width: "100%" }} cols={6}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <div className="poster">
+              <img
+                src={`${item.img}?w=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"/>
+            </div>
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
-  
-    <img src="https://assets.codepen.io/1025350/newfoundglory.png" alt="New Found Glory tour poster: 20 years of pop punk" />
-  
-    <div className="poster">
-      <img src="https://assets.codepen.io/1025350/newfoundglory.png" alt="New Found Glory tour poster: 20 years of pop punk" />
-    </div>
-  
-    <img src="https://assets.codepen.io/1025350/paper-heart.png" alt="movie poster for Charlyne Yi’s Paper Heart" />
-  
-    <div className="poster">
-      <img src="https://assets.codepen.io/1025350/paper-heart.png" alt="movie poster for Charlyne Yi’s Paper Heart" />
-    </div>
-  
-  </div>)
+  )
   }
 }
 
